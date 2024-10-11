0360c67e162b03480e1d896ed126b283b7784103
nikhil <nikhilsahani242003@gmail.com>
2024-10-11 14:03:30 +0530

    new

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,22 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
 const registerUser = asyncHandler(async (req, res) => {
   const { fullname, email, password, username } = req.body;
   console.log("email", email);
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "all fields are required");
+  }
+  const existedUser = User.findOne({
+    $or: [{ email }, { username }],
+  });
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exist");
+  }
+  const avatarLocalPath = req.files?avatar[0]?.path
+
+
 });
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,16 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-const router =  Router()
-router.route("/register").post(registerUser)
+import { upload } from "../middlewares/multer.middleware.js";
+const router = Router();
+router.route("/register").post(upload.fields([{
+    name:"avatar",
+    maxCount:1
 
-export default router
+}, 
+{
+    name:"coverImage",
+    maxCount:1
+
+}]), registerUser);
+
+export default router;
